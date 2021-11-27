357b1060fff027cdd8aea2c21c4b0baebb9fba71
kubilaykaand <kubilaykaand@gmail.com>
2021-11-27 18:15:57 +0300

    Initial commit

---
+++ main.py
@@ -0,0 +1,12 @@
+sentence = "What is the Airspeed Velocity of an Unladen Swallow?"
+# Don't change code above 👆
+
+# Write your code below:
+for word in sentence.split():
+    print(word)
+result={string:len(string) for string in sentence.split()}
+#result={new_key:new_value for word in sentence.split()}
+
+
+
+print(result)
